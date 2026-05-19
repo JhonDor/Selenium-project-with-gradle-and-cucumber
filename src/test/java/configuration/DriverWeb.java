20711ead889c707e8606b830dab7913e9587d49f
jhon.dorado <jhon.dorado@globant.com>
2026-05-19 14:20:26 -0500

    Fix GitHub Actions: Install Chrome and enable headless mode

--- src/test/java/configuration/DriverWeb.java
+++ src/test/java/configuration/DriverWeb.java
@@ -16,11 +16,22 @@ public class DriverWeb {
         /**
          * Constructor method.
          * Only Chrome browser is defined.
+         * Supports headless mode via system property for CI/CD environments.
          */
         public DriverWeb() {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--incognito");
-            //ChromeDriver driver = new ChromeDriver(options);
+            
+            // Enable headless mode for CI environments (GitHub Actions, etc.)
+            String headlessMode = System.getProperty("headlessMode", "false");
+            if ("true".equalsIgnoreCase(headlessMode)) {
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--window-size=1920,1080");
+            }
+            
             WebDriverManager.chromedriver().setup();
             this.driver = new ChromeDriver(options);
         }
